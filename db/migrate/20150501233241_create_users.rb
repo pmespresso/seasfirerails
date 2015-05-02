7cf55c749767808323fd9b2720f56b924611cc58
YJ Kim <yjkimjunior@berkeley.edu>
2015-05-02 01:37:20 -0700

    styling logs page

--- db/migrate/20150501233241_create_users.rb
+++ db/migrate/20150501233241_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :username, :email
-    	t.timestamps
+    	t.string :username
+    	
     end
   end
 end
