12bdfbb2ef3607570ec3b2d81fb324db05b2c0a9
YJ Kim <yjkimjunior@berkeley.edu>
2015-05-05 10:22:56 -0700

    styled log show tbc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502020713) do
+ActiveRecord::Schema.define(version: 20150505162300) do
 
   create_table "logs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150502020713) do
     t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
